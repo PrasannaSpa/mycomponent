0dc5190d80a8230ec382163947d4e1383ef77182
Prasanna <prasannaabcd32881@gmail.com>
2024-02-26 17:27:26 +0530

    sec

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import MyReactComponent from "./MyReactComponent";
+import MyReactComponent from "./MyReactComponent";
 function App() {
-  return <div className="App">{/* <MyReactComponent /> */}</div>;
+  return (
+    <div className="App">
+      <MyReactComponent />
+    </div>
+  );
 }
 
 export default App;
--- src/MyReactComponent.js
+++ src/MyReactComponent.js
@@ -1,5 +1,30 @@
 import React from "react";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
 
 export default function MyReactComponent() {
-  return <div>MyReactComponent by abcd</div>;
+  return (
+    <div style={{ position: "relative", height: "500px" }}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "just now",
+                sender: "Joe",
+              }}
+            />
+          </MessageList>
+          <MessageInput placeholder="Type message here" />
+        </ChatContainer>
+      </MainContainer>
+    </div>
+  );
 }
